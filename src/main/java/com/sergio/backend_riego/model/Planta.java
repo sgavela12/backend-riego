f79f877b40db8e7f0f54075a1598256721f21a23
sgavela12 <sgavela12@gmail.com>
2025-03-08 21:28:33 +0100

    Agregar anotaciones de Lombok y constructores a las entidades del modelo

--- src/main/java/com/sergio/backend_riego/model/Planta.java
+++ src/main/java/com/sergio/backend_riego/model/Planta.java
@@ -1,9 +1,24 @@
 package com.sergio.backend_riego.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "planta")
 public class Planta {
 
--- src/main/java/com/sergio/backend_riego/model/SensorData.java
+++ src/main/java/com/sergio/backend_riego/model/SensorData.java
@@ -9,8 +9,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "sensor_data")
 public class SensorData {
 
