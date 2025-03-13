97e9967eb09a1d831b7928a3ef756c591f645996
Sergio Gavela <sgavela12@gmail.com>
2025-03-13 19:05:18 +0100

    quito websocket

--- src/main/java/com/sergio/backend_riego/BackendRiegoApplication.java
+++ src/main/java/com/sergio/backend_riego/BackendRiegoApplication.java
@@ -2,10 +2,8 @@ package com.sergio.backend_riego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
-@EnableWebSocket
 public class BackendRiegoApplication {
 
 	public static void main(String[] args) {
