9d71f7c0498343efc22d112f04d8695102c3e092
Sergio Gavela <sgavela12@gmail.com>
2025-04-11 18:45:49 +0200

    Mejorar la respuesta de Alexa al consultar la humedad, incluyendo el nombre de la planta y manejando casos de plantas desconocidas.

--- src/main/java/com/sergio/backend_riego/controller/AlexaController.java
+++ src/main/java/com/sergio/backend_riego/controller/AlexaController.java
@@ -4,15 +4,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.sergio.backend_riego.model.Planta;
+import com.sergio.backend_riego.service.PlantaService;
+
 @RestController
 @RequestMapping("/alexa")
 public class AlexaController {
+
+    @Autowired
+    private PlantaService plantaService; // Servicio para buscar plantas en la base de datos
 
     @PostMapping("/consultar-humedad")
     public Map<String, Object> consultarHumedad() {
@@ -25,13 +32,22 @@ public class AlexaController {
         List<Map<String, Object>> sensores = (List<Map<String, Object>>) response.getBody().get("sensores");
 
         // Armar mensaje para Alexa
-        StringBuilder mensaje = new StringBuilder("Las humedades actuales son: ");
+        StringBuilder mensaje = new StringBuilder("Las humedades actuales de las plantas son: ");
         for (Map<String, Object> sensor : sensores) {
-            mensaje.append("Sensor ")
-                   .append(sensor.get("id"))
-                   .append(" con ")
-                   .append(sensor.get("humedad"))
-                   .append(" por ciento. ");
+            Long plantaId = ((Number) sensor.get("id")).longValue(); // Obtener el ID de la planta
+            Planta planta = plantaService.getPlantaById(plantaId).orElse(null); // Buscar la planta en la base de datos
+
+            if (planta != null) {
+                mensaje.append(planta.getNombre()) // Usar el nombre de la planta
+                       .append(" con ")
+                       .append(sensor.get("humedad"))
+                       .append(" por ciento, ");
+            } else {
+                mensaje.append("Planta desconocida") // Si no se encuentra la planta
+                       .append(" con ")
+                       .append(sensor.get("humedad"))
+                       .append(" por ciento, ");
+            }
         }
 
         // Estructura de respuesta para Alexa
